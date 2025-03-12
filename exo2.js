8bde97d70b3762382ecf6d7d25ab3e4e5ce98f5f
Carine <carinerandrianarijaona@gmail.com>
2025-03-12 09:48:44 +0100

    Initialisation

---
+++ exo2.js
@@ -0,0 +1,1 @@
+/* Cette fois-ci, on va utiliser les paramètres de requête pour créer une page web. Tu peux par exemple implémenter une recherche ou une pagination. N’hésite pas à demander des idées aux encadrant·es ou autres apprenant·es de ta promotion. */
